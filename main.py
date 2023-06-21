b1226881a79f96a23e765c5cc541f2773008ca07
Ali <46873759+Ghoreish@users.noreply.github.com>
2023-06-21 12:17:23 +0330

    Update main.py
    I just made it shorter

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import random
-a=random.randint(0,1)
-l=''
+
+
+def guess_next(history: list):
+    total_len = len(history)
+    for i in range(1, total_len):
+        selected = history[i: total_len]
+        start = i - 1
+        end = total_len - 1
+        while start >= 0:
+            if selected == history[start: end]:
+                print(selected, history[start: end + 1])
+                print(history)
+                return history[end]
+            start -= 1
+            end -= 1
+    return None
+
+
+
+history = []
+guess = random.randint(0, 1)
 while True:
-    print("I guess your number is:",a)
-    print("Is this your number?(y/n):",end=" ")
-    x=input()
-    if x=="y" or x=="Y":
-        l+=str(a)
-    elif x=="n" or x=="N":
-        l+=str((a+1)%2)
+    print('Is your number {}? (1/0)(yes/no)'.format(str(guess)))
+    inp = input('> ')
+    if inp == '1':
+        history.append(guess)
+    elif inp == '0':
+        history.append((guess + 1) % 2)
     else:
-        print("ERROR!")
+        print('Wrong input BRO!!!')
         continue
-    m=0
-    c = 0
-    if len(l)>=3:
-        for i in range(1,len(l)-1):
-            if l[i:] in l[:-1]:
-                c=1
-                if l[i:]+"1" in l and l[i:]+"0" in l:
-                    p=0
-                    while l[i:]+"1" in l[p:] and l[i:]+"0" in l[p:]:
-                        p+=1
-                    if l[i:]+"1" in l[p:]:
-                        a=1
-                    else:
-                        a=0
-                else:
-                    if l[i:]+"1" in l:
-                        a=1
-                    else:
-                        a=0
-                break
-    if c==0:
-        a=random.randint(0,1)
+
+    next = guess_next(history)
+    print(next)
+    if next != None:
+        guess = next
+    else:
+        guess = random.randint(0, 1)
+
